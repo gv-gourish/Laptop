f1866ebb4e5a455257a4735bc41c253b2e58889b
Gourish <gourish.devops@gmail.com>
2022-02-19 10:54:58 +0530

    Revert information

---
+++ e.sh
@@ -0,0 +1,3 @@
+Git revert wrking properly
+if you revert changes are removed nt only from the locall repo also frm the Github repository
+after the revert agan one more push perations needs t be done
